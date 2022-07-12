2df620617aeaa1c900d7f14818bf06cb27d7def5
Michael-ugorji <ugorjimicheal.um@gmail.com>
2022-07-12 23:05:22 +0100

    Navbar Menu Open and Close Done

--- src/components/navbar/mobile-nav-menu/MobileNav.styled.jsx
+++ src/components/navbar/mobile-nav-menu/MobileNav.styled.jsx
@@ -2,19 +2,19 @@ import tw from "tailwind-styled-components";
 // import styled from "styled-components";
 
 export const MobileMenuDropDown = tw.div`
-absolute 
-left-[-100%]
-top-0 
 bg-gray-100/90
 w-10/12
-md:w-6/12 
-lg:hidden
+md:w-6/12
+lg:hidden 
 px-4
-py-5 
+py-2 
 flex 
 flex-col
 ease-in duration-300
 z-30
+left-0-[-100%]
+absolute
+top-0
 `;
 
 export const Button = tw.button`
