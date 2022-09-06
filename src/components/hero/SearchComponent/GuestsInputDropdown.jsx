d861bde62c68f62e8c5c834ea1dde8f33f4d587b
Izunnaya <ugorjimicheal.um@gmail.com>
2022-09-06 16:09:50 +0100

    GuestDropdwon Adjustment

--- src/components/hero/SearchComponent/GuestsInputDropdown.jsx
+++ src/components/hero/SearchComponent/GuestsInputDropdown.jsx
@@ -5,15 +5,15 @@ const GuestsInput = ({ guestHandler, selectGuest }) => {
   // The arguments passed in the "guestHandler" as values in the function are descrided as follows: "d: for decrease & i:Increase"
 
   return (
-    <div className="absolute top-[63px] w-full bg-stone-100 rounded-md left-0 p-5 cursor-default">
-      <div className="flex justify-between items-center text-center mb-5  text-xl font-light">
+    <div className="absolute top-[63px] w-full bg-stone-100 rounded-md left-0 p-4 cursor-default">
+      <div className="flex justify-between items-center text-center mb-5  text-[17px] font-light">
         <div>
           <h4>Adults</h4>
         </div>
 
         <div className="cursor-pointer text-base flex justify-center items-center">
           <button
-            className="rounded-full bg-slate-200 p-3"
+            className="rounded-full bg-slate-200 p-2"
             onClick={() => guestHandler("adults", "d")}
             disabled={selectGuest.adults <= 1}
             type={"button"}
@@ -24,7 +24,7 @@ const GuestsInput = ({ guestHandler, selectGuest }) => {
             {selectGuest.adults}
           </span>
           <button
-            className="rounded-full bg-slate-200 p-3"
+            className="rounded-full bg-slate-200 p-2"
             onClick={() => guestHandler("adults", "i")}
             type={"button"}
           >
@@ -32,14 +32,14 @@ const GuestsInput = ({ guestHandler, selectGuest }) => {
           </button>
         </div>
       </div>
-      <div className="flex justify-between items-center text-center my-3 pt-3  text-xl font-light">
+      <div className="flex justify-between items-center text-center my-3 pt-3  text-[17px] font-light">
         <div>
           <h4>Children</h4>
         </div>
 
         <div className="cursor-pointer text-base flex justify-center items-center">
           <button
-            className="rounded-full bg-slate-200 p-3"
+            className="rounded-full bg-slate-200 p-2"
             onClick={() => guestHandler("children", "d")}
             disabled={selectGuest.children <= 0}
             type={"button"}
@@ -51,7 +51,7 @@ const GuestsInput = ({ guestHandler, selectGuest }) => {
             {selectGuest.children}
           </span>
           <button
-            className="rounded-full bg-slate-200 p-3"
+            className="rounded-full bg-slate-200 p-2"
             onClick={() => guestHandler("children", "i")}
             type={"button"}
           >
